512d23c4dfb5688aed0b8b79fefd3c01b0221981
patmood <patrick.n.moody@gmail.com>
2015-07-11 12:44:43 -0700

    only show hand once rotated

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // Credit: http://demosthenes.info/blog/943/An-SVG-Analog-Clock-In-6-Lines-of-JavaScript
+
+var hourHand = document.getElementById('hour')
+
 function r(el, deg) {
   el.setAttribute('transform', 'rotate('+ deg +' 50 50)')
 }
 
 function setRotation() {
   var d = new Date()
+    , mins = d.getMinutes()
+    , hours = d.getHours()
 
-  r(hour, 15*(d.getHours()) + d.getMinutes()/4 + 180)
+  r(hourHand, 15*(hours) + mins/4 + 180)
 
-  document.title = d.getHours() + ':' + d.getMinutes()
+  document.title = hours + ':' + mins
 }
 
 // Initial set
 setRotation()
+
+// Show hand once rotated
+hourHand.style.visibility = 'visible'
 
 // Continuously update
 setInterval(function() {
